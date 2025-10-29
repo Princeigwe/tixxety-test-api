90562941b2eda932f3ed227a3a21d5012c22a95b
Prince Igwe <igwep297@gmail.com>
2025-10-29 15:12:26 +0100

    docker compose services setup

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.12-slim-bookworm
+ENV PYTHONUNBUFFERED=1
+WORKDIR /code
+COPY ./requirements.txt /code/requirements.txt
+RUN pip install -r /code/requirements.txt
+COPY . /code
+EXPOSE 8000
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ database_config.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+Base = declarative_base()
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
