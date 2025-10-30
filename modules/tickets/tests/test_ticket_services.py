cd302d5c620adb7ec2d701d22228a6928c7834b0
Prince Igwe <igwep297@gmail.com>
2025-10-30 09:49:00 +0100

    unittest for ticket reservation logic

--- modules/tickets/tests/test_ticket_services.py
+++ modules/tickets/tests/test_ticket_services.py
@@ -46,21 +46,55 @@ mock_ticket_database = [
     "created_at": "2025-10-29T22:17:39.524362",
     "id": "c5c8741a-a58c-4b4d-9e3e-7562731e7ab4",
     "user_id": "409f9108-891f-42a1-99e9-d3e9e33ce615"
-  },
-  {
+  }
+]
+
+
+mock_api_ticket_response ={
     "event_id": "89efa541-3860-46a8-a8b8-f9fd6760c3a0",
     "status": "reserved",
     "created_at": "2025-10-29T22:18:10.345678",
     "id": "d2f5e1c3-4b6e-4c2a-9f7e-123456789abc",
     "user_id": "409f9108-891f-42a1-99e9-d3e9e33ce615"
   }
-]
 
 
 def test_reserve_ticket_event_not_found():
-    response = client.post(
-        "/tickets/",
-        headers={"Authorization": mock_bearer_token},
-        json={"event_id": "non-existing-event-id"}
-    )
-    assert response.status_code == 404
+  response = client.post(
+    "/tickets/",
+    headers={"Authorization": mock_bearer_token},
+    json={"event_id": "non-existing-event-id"}
+  )
+  assert response.status_code == 404
+
+
+def test_reserve_ticket_already_reserved():
+  response = client.post(
+    "/tickets/",
+    headers={"Authorization": mock_bearer_token},
+    json={"event_id": "4b45e744-1416-41ff-ae4b-f7345f57a8c4"}
+  )
+  event_id = "4b45e744-1416-41ff-ae4b-f7345f57a8c4"
+  user_id_to_check = "4409f9108-891f-42a1-99e9-d3e9e33ce615"
+
+  # checking if the user has already reserved a ticket for the event
+  for ticket in mock_ticket_database:
+    if ticket["event_id"] == event_id and ticket["user_id"] == user_id_to_check:
+      assert response.status_code == 400
+
+
+def test_reserve_ticket_success():
+  response = client.post(
+    "/tickets/",
+    headers={"Authorization": mock_bearer_token},
+    json={"event_id": "89efa541-3860-46a8-a8b8-f9fd6760c3a0"}
+  )
+  event_id = "89efa541-3860-46a8-a8b8-f9fd6760c3a0"
+  user_id_to_check = "4409f9108-891f-42a1-99e9-d3e9e33ce615"
+
+  # checking if the user has already reserved a ticket for the event
+  for ticket in mock_ticket_database:
+    if ticket["event_id"] == event_id and ticket["user_id"] == user_id_to_check:
+      assert response.status_code == 200
+  
+
